7eaf7f9377a118adf06b4590436a95aae237d497
rrandom <emanonhere@gmail.com>
2019-05-05 23:18:52 +0800

    smush_horizontal

--- src/rules.rs
+++ src/rules.rs
@@ -9,10 +9,28 @@ pub struct Rules {
 }
 
 impl Rules {
-    fn smushes_horizontal(char1: char, char2: char, hardblank: char) {
-        unimplemented!()
+    pub fn smushes_horizontal(&self, char1: char, char2: char, hardblank: char) -> bool {
+        self.horizontal_rules
+            .iter()
+            .any(|r| r.smush(char1, char2, hardblank).is_some())
     }
-    fn smush_horizontal(char1: char, char2: char, hardblank: char) {
-        unimplemented!()
+    pub fn smush_horizontal(&self, char1: char, char2: char, hardblank: char) -> Option<char> {
+        if char1 == ' ' {
+            return Some(char2);
+        }
+        if char2 == ' ' {
+            return Some(char1);
+        }
+
+        if self.horizontal_layout == LayoutMode::UniversalSmush {
+            return SmushingRule::HorizontalSmushing.smush(char1, char2, hardblank);
+        }
+        for r in self.horizontal_rules.iter() {
+            let smush = r.smush(char1, char2, hardblank);
+            if smush.is_some() {
+                return smush;
+            }
+        }
+        None
     }
 }
